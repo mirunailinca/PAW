93c7ea6e11669c46f0fa1962cbe888c3ad1a8c9f
mirunailinca <ilinca.miruna@icloud.com>
2024-02-29 15:21:39 +0200

    paw-seminar2

---
+++ seminar2-paw/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seminar2_paw {
+    internal class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
